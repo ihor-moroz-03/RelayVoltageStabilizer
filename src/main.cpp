8851dcbceb6408c59e12c7f9bae0789d48b4fe80
Ihor Moroz <ihor.moroz.17@gmail.com>
2022-01-24 09:30:54 +0200

    relay mode setting safety

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,15 @@ constexpr uint8_t delayLedPin = 6;
 constexpr uint8_t networkConnectionLedPin = 7;
 constexpr uint8_t protectionLedPin = 13;
 
-constexpr uint8_t relay1 = 10;
-constexpr uint8_t relay2 = 11;
-constexpr uint8_t relay3 = 12;
+constexpr uint8_t relays[3] = { 10, 11, 12 };
 constexpr uint8_t outputRelay = 9;
 constexpr uint8_t modesMap[6] = { 0, 1, 4, 5, 2, 3 };
 
-constexpr unsigned long delayTimeout = 5000;
-constexpr int jumpProtectionBoundary = 8;
-constexpr int outputProtectionBottom = 195;
-constexpr int outputProtectionTop = 245;
-constexpr int relaySwitchDelay = 225;
+constexpr unsigned long delayTimeout = 10000;
+constexpr int jumpProtectionBoundary = 3;
+constexpr int outputProtectionBottom = 220 - jumpProtectionBoundary;
+constexpr int outputProtectionTop = 240 + jumpProtectionBoundary;
+constexpr int relaySwitchDelay = 150;
 
 static uint8_t relayMode;
 
@@ -29,9 +27,7 @@ bool isOutputVoltageSafe();
 
 void setup()
 {
-    pinMode(relay1, OUTPUT);
-    pinMode(relay2, OUTPUT);
-    pinMode(relay3, OUTPUT);
+    for (int i = 0; i < 3; ++i) pinMode(relays[i], OUTPUT);
     pinMode(outputRelay, OUTPUT);
     pinMode(protectionLedPin, OUTPUT);
     pinMode(delayLedPin, OUTPUT);
@@ -72,16 +68,32 @@ void loop()
     digitalWrite(protectionLedPin, !isOutputAllowed);
 }
 
+void decomposeRelayMode(uint8_t mode, uint8_t* output)
+{
+    for (int i = 2; i > -1; --i, mode >>= 1)
+        output[i] = mode & 1;
+}
+
+void relaysDigitalWrite(uint8_t* targetPositions, bool (*predicate)(uint8_t))
+{
+    static constexpr uint8_t relayPriority[3] = { 1, 0, 2 };
+    for (uint8_t i : relayPriority)
+        if (predicate(targetPositions[i]) && digitalRead(relays[i]) != targetPositions[i])
+        {
+            digitalWrite(relays[i], targetPositions[i]);
+            delay(relaySwitchDelay);
+        }
+}
+
 void setRelayMode(uint8_t mode)
 {
+    uint8_t targetPositions[3];
+    decomposeRelayMode(modesMap[mode], targetPositions);
+
+    relaysDigitalWrite(targetPositions, [](uint8_t pos) { return pos == 1; });
+    relaysDigitalWrite(targetPositions, [](uint8_t pos) { return pos == 0; });
+
     relayMode = mode;
-
-    mode = modesMap[mode];
-    digitalWrite(relay3, mode & 1);
-    digitalWrite(relay2, (mode >>= 1) & 1);
-    digitalWrite(relay1, (mode >>= 1) & 1);
-
-    delay(relaySwitchDelay);
 }
 
 void toggleOutputRelay(uint8_t state)
